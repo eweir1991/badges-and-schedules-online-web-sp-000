0cfd9f8c5056825a23f9d119b0dcfa9993b9bbe3
Erik Weir <erik.weir@key.me>
2020-05-28 19:46:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,26 +1,3 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    badge_maker(name)
-  end
-end
-  
-def assign_rooms(attendees)
-  room=0
-  attendees.collect do |name|
-    room+=1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
-  end
-end
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
-  end
-  assign_rooms(attendees).each do |value|
-    puts value
-  end
+  return "Hello, my name is #{name}."
 end
