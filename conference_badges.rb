288fd88a0792a0bacbc92017b6fa5a1ad962a6fe
Erik Weir <erik.weir@key.me>
2020-05-28 19:52:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,13 @@ def batch_badge_creator(array)
   end
   return new_array
 end
+
+def assign_rooms(array)
+  new_array = []
+  counter = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assinged to room #{counter}!")
+    counter += 1
+  end
+  return new_array
+end
